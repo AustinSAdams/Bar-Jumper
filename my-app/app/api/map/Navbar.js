d9a7a9a37151a172f08e399b5498f2ab07278e65
IIITreyIII <132967114+IIITreyIII@users.noreply.github.com>
2024-10-16 20:28:19 -0500

    Update Navbar.js

--- my-app/app/api/map/Navbar.js
+++ my-app/app/api/map/Navbar.js
@@ -8,7 +8,7 @@ const NavBar = ({ theme }) => {
       <div className="navbar-content">
         <NavItem icon={<Map size={24} />} label="Map" />
         <NavItem icon={<Search size={24} />} label="Search" />
-        <NavItem icon={<List size={24} />} label="Bar List" />
+        <NavItem icon={<List size={24} />} label="Bars" />
         <NavItem icon={<MessageSquare size={24} />} label="Chat" />
         <NavItem icon={<User size={24} />} label="Profile" />
       </div>
