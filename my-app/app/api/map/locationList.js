be6b0adeb6f034ba8adc766aa167c425f9ab64fd
IIITreyIII <132967114+IIITreyIII@users.noreply.github.com>
2024-10-17 01:23:44 -0500

    nav-bar-locList
    a pretty mint version of the nav bar. I have a good template for building. I have been staring at vs vode for 7 hours!

--- my-app/app/api/map/locationList.js
+++ my-app/app/api/map/locationList.js
@@ -1,17 +1,36 @@
-// LocationList.js
 import React from 'react';
 import { X } from 'lucide-react';
 import './locationList.css';
+
+const renderStars = (starCount) => {
+    return Array.from({ length: 5 }, (_, index) => {
+      const fullStarThreshold = index + 1;
+      
+      if (starCount >= fullStarThreshold) {
+        return <span key={index} className="star filled">★</span>;
+
+      } else if (starCount > index && starCount < fullStarThreshold) {
+        return <span key={index} className="star half">★</span>;
+
+      } else {
+        return <span key={index} className="star">★</span>;
+      }
+    });
+  };
 
 const LocationList = ({ locations, onSelectLocation, onClose, theme }) => {
   return (
     <div className={`location-popup-content ${theme === 'dark' ? 'dark-mode' : ''}`}>
       <div className="location-popup-header">
-        <h2 className={`location-popup-title ${theme === 'dark' ? 'dark-mode' : ''}`}>All Locations</h2>
+
+        <h2 className={`location-popup-title ${theme === 'dark' ? 'dark-mode' : ''}`}>Bar Locations</h2>
+
         <button onClick={onClose} className={`location-popup-close ${theme === 'dark' ? 'dark-mode' : ''}`}>
           <X size={24} />
         </button>
+
       </div>
+
       <div className="location-list">
         {locations.map(location => (
           <div 
@@ -20,9 +39,18 @@ const LocationList = ({ locations, onSelectLocation, onClose, theme }) => {
             onClick={() => onSelectLocation(location)}
           >
             <h3>{location.name}</h3>
+
             <p>{location.address}</p>
+
+            <div className="location-rating">
+              {renderStars(location.starCount || 0)} {/*  starCount from Firebase */}
+            </div>
+
           </div>
+
         ))}
+
+
       </div>
     </div>
   );
