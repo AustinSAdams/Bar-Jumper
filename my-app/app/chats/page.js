cdc9881fe41201cc1f1c838a02a533a14c63b94c
ChaseStephens3 <ChaseS4365@gmail.com>
2024-10-27 21:42:00 -0500

    Chatroom-Setup-Dev
    Allows users to view the various chatrooms and send messages within them. Next Sprint: Live chats between multiple users.

--- my-app/app/chats/page.js
+++ my-app/app/chats/page.js
@@ -1,3 +1,4 @@
+"use client";
 /* This page is for the "/chats" extension on the website. It should have:
     1. Default Theme Colors.
     2. Easy to navigate menu.
@@ -7,11 +8,22 @@
     To access this page locally, after running "npm run dev", go into your web browser
     and type in "http://localhost:3000/chats".
 */
+import React from 'react';
+import Chat from './Chat';
+
+const chatrooms = [
+    { id: 1, name: 'Dawg House' },
+    { id: 2, name: 'The Revelry' },
+    { id: 3, name: 'Sundown Tavern' },
+    { id: 4, name: 'Ponchatoulas'},
+    { id: 5, name: 'Bayou Axe Ruston'},
+    { id: 6, name: 'Utility Brewing Co'}
+];
 
 export default function Page(){
     return (
     <div>
-        <p>Chats Page</p>
+        <Chat chatrooms={chatrooms} />
     </div>
     );
 }
